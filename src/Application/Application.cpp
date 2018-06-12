6288d7476d3cc59c46929dadb6541955e2f64ce3
gammasoft71 <gammasoft71@gmail.com>
2018-06-12 18:39:58 +0200

    Create

---
+++ src/Application/Application.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  if (message == WM_DESTROY) PostQuitMessage(0);
+  return DefWindowProc(hwnd, message, wParam, lParam);;
+}
+
+int main(int argc, char* argv[]) {
+  HWND form = CreateWindowEx(0, WC_DIALOG, L"", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
+  SetWindowLongPtr(form, GWLP_WNDPROC, (LONG_PTR)WndProc);
+  ShowWindow(form, SW_SHOW);
+
+  MSG message;
+  while (GetMessage(&message, nullptr, 0, 0)) {
+    TranslateMessage(&message);
+    DispatchMessage(&message);
+  }
+  return (int)message.wParam;
+}
---
+++ src/Application/README.md
@@ -0,0 +1,5 @@
+# Application
+
+This example demonstrates the use of Gtk::Application class.
+
+![GitHub Logo](../../docs/Pictures/Application.png)
