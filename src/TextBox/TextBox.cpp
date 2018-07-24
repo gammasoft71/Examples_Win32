a92bf6bd65065b1d88d65d3af5388367924fd289
gammasoft71 <gammasoft71@gmail.com>
2018-07-24 10:11:48 +0200

    add TextBox

--- src/TextBox/TextBox.cpp
+++ src/TextBox/TextBox.cpp
@@ -5,6 +5,7 @@
 
 HWND form;
 HWND textBox1;
+HWND textBox2;
 
 WNDPROC defWndProc;
 
@@ -16,6 +17,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 int main(int argc, char* argv[]) {
   form = CreateWindowEx(0, WC_DIALOG, L"TextBox example", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
   textBox1 = CreateWindowEx(WS_EX_CLIENTEDGE, WC_EDIT, L"textBox1", WS_CHILD | WS_VISIBLE, 10, 10, 100, 23, form, nullptr, nullptr, nullptr);
+  textBox2 = CreateWindowEx(WS_EX_CLIENTEDGE, WC_EDIT, L"textBox2", WS_CHILD | WS_VISIBLE, 10, 50, 100, 23, form, nullptr, nullptr, nullptr);
 
   defWndProc = (WNDPROC)SetWindowLongPtr(form, GWLP_WNDPROC, (LONG_PTR)WndProc);
 
