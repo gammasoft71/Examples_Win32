d07bac0fb6076b8caf168e147a9cee5ca4f57a1f
gammasoft71 <gammasoft71@gmail.com>
2018-08-23 17:15:17 +0200

    rename src to Win32.Gui and add Win32.System

---
+++ Win32.System/Console/Console.cpp
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Hello, World!\n", 14, nullptr, nullptr);
+}
---
+++ Win32.System/ConsoleColor/ConsoleColor.cpp
@@ -0,0 +1,52 @@
+﻿#include <cstdint>
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                                             ▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                      ▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                                                \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                                                \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                      ▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                                  ▓▓▓▓▓▓▓▓▓▓▓▓▓ \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                                                                \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"                                   Win32 Api                                    \n", 81, nullptr, nullptr);
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY | FOREGROUND_INTENSITY);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Using the Windows API, you can develop applications that run successfully on all\n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"versions of Windows while taking advantage of the features and capabilities     \n", 81, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"unique to each version.                                                         \n", 81, nullptr, nullptr);
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
