cab75411d68c2131b66473e44684dc10ba420cd1
gammasoft71 <gammasoft71@gmail.com>
2018-08-24 00:55:44 +0200

    add HelloWorld project

---
+++ Win32.Gui/HelloWorldForm/README.md
@@ -0,0 +1,1 @@
+﻿﻿# HelloWorldFormThe classic first application "Hello World".# Source[HelloWorldForm.cpp](./HelloWorldForm.cpp)[CMakeLists.txt](./CMakeLists.txt)# Output![GitHub Logo](../../docs/Pictures/HelloWorld.png)
--- Win32.System/Console/Console.cpp
+++ Win32.System/Console/Console.cpp
@@ -1,5 +1,24 @@
+#include <cstdint>
+#include <string>
 #include <Windows.h>
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
-  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Hello, World!\n", 14, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Hello ", 6, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"World!\n", 7, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Enter your name: ", 17, nullptr, nullptr);
+  wstring name(1024, 0);
+  int32_t numberOfCharsRead = 0;
+  ReadConsole(GetStdHandle(STD_INPUT_HANDLE), (LPVOID)name.c_str(), 1024, (LPDWORD)&numberOfCharsRead, nullptr);
+  name.resize(numberOfCharsRead-2);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Good day, ", 10, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), name.c_str(), name.length(), nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"!\n", 2, nullptr, nullptr);
 }
+
+// This code produces the following output:
+//
+// Hello World!
+// Enter your name: James
+// Good day, James!
