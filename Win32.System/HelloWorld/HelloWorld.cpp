c840227d2fd3a92e52e82e1cb8b0c8c519cf0320
gammasoft71 <gammasoft71@gmail.com>
2019-02-04 23:37:08 +0100

    Fix error

--- Win32.System/HelloWorld/HelloWorld.cpp
+++ Win32.System/HelloWorld/HelloWorld.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
 
 int main(int argc, char* argv[]) {
-  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Hello, World!\n", 14, nullptr, nullptr);  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Hello, World!\n", 14, nullptr, nullptr);
+  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), L"Hello, World!\n", 14, nullptr, nullptr);
 }
