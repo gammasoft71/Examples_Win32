bab6a99a1f4c64db0817d47176b0dd389b8a868c
gammasoft71 <gammasoft71@gmail.com>
2019-02-04 23:40:13 +0100

    Add Process example

---
+++ Win32.System/Process/Process.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
+  STARTUPINFO startupInfo;
+  PROCESS_INFORMATION processInfo;
+
+  ZeroMemory(&startupInfo, sizeof(startupInfo));
+  startupInfo.cb = sizeof(startupInfo);
+  ZeroMemory(&processInfo, sizeof(processInfo));
+
+  std::wstring name = L"explorer c:\\Users\\yves\\Desktop\\sample.txt";
+  if (CreateProcess(nullptr, (wchar_t*)name.c_str(), nullptr, nullptr, false, 0, nullptr, nullptr, &startupInfo, &processInfo) == 0) {
+    std::cout << "CreateProcess failed " << GetLastError() << std::endl;
+    return 1;
+  }
+
+  /*
+  if (!CreateProcess(L"cmd /c dir", nullptr, nullptr, nullptr, false, 0, nullptr, nullptr, &startupInfo, &processInfo)) {
+    std::cout << "CreateProcess failed " << GetLastError() << std::endl;
+    return 1;
+  }*/
+  WaitForSingleObject(processInfo.hProcess, INFINITE);
+  return 0;
+}
---
+++ Win32.System/Process/README.md
@@ -0,0 +1,13 @@
+# Application
+
+This example demonstrates the use of GetMessage and DispatchMessage loop.
+
+# Source
+
+[Application.cpp](./Application.cpp)
+
+[CMakeLists.txt](./CMakeLists.txt)
+
+# Output
+
+![GitHub Logo](../../docs/Pictures/Application.png)
