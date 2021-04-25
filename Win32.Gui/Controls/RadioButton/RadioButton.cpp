49d50676486eeceda69de8f83abebdc95e27bc0e
gammasoft71 <gammasoft71@gmail.com>
2021-04-25 22:15:14 +0200

    Update RadioButton.cpp

--- Win32.Gui/Controls/RadioButton/RadioButton.cpp
+++ Win32.Gui/Controls/RadioButton/RadioButton.cpp
@@ -1,11 +1,10 @@
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-#include <string>
-#include <Windows.h>
-#include <CommCtrl.h>
 #include <initializer_list>
 #include <map>
 #include <string>
+#include <Windows.h>
+#include <CommCtrl.h>
 
 using namespace std;
 using namespace std::literals;
