7247c744de4d0e9ea68ca31a37f6acc0fefe6baa
gammasoft71 <gammasoft71@gmail.com>
2021-04-25 22:41:15 +0200

    Update TextBox.cpp

--- Win32.Gui/Controls/TextBox/TextBox.cpp
+++ Win32.Gui/Controls/TextBox/TextBox.cpp
@@ -12,20 +12,30 @@ HWND textBox1 = nullptr;
 HWND textBox2 = nullptr;
 WNDPROC defWndProc = nullptr;
 
+LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  PostQuitMessage(0);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
 std::wstring GetText(HWND hwnd) {
-  wstring text(SendMessage(hwnd, WM_GETTEXTLENGTH, 0, 0) + 1, '\0');
-  SendMessage(hwnd, WM_GETTEXT, (WPARAM)text.size(), (LPARAM)text.c_str());
+  size_t size = SendMessage(hwnd, WM_GETTEXTLENGTH, 0, 0);
+  if (!size) return L"";
+  wstring text(size, '\0');
+  SendMessage(hwnd, WM_GETTEXT, (WPARAM)text.size() + 1, (LPARAM)text.c_str());
   return text;
 }
 
+LRESULT OnTextChanged(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  wstring text = GetText((HWND)lParam);
+  for (auto textBox : {textBox1, textBox2})
+    if (GetText(textBox) != text)
+      SendMessage(textBox, WM_SETTEXT, 0, (LPARAM)text.c_str());
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  if (message == WM_COMMAND && HIWORD(wParam) == EN_CHANGE) {
-    wstring text = GetText((HWND)lParam);
-    HWND otherTextBox = (HWND)lParam == textBox1 ? textBox2 : textBox1;
-    if (GetText(otherTextBox) != text)
-      SendMessage(otherTextBox, WM_SETTEXT, 0, (LPARAM)text.c_str());
-  }
-  if (message == WM_CLOSE && hwnd == window) PostQuitMessage(0);
+  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
+  if (message == WM_COMMAND && HIWORD(wParam) == EN_CHANGE) return OnTextChanged(hwnd, message, wParam, lParam);
   return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
 }
 
