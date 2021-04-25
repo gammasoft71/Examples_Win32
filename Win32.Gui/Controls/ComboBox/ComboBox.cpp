ec1df8d6d7b46dd3c74285a07ea8e5e3fa79c66c
gammasoft71 <gammasoft71@gmail.com>
2021-04-25 20:31:08 +0200

    Update ComboBox.cpp

--- Win32.Gui/Controls/ComboBox/ComboBox.cpp
+++ Win32.Gui/Controls/ComboBox/ComboBox.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-HWND form = nullptr;
+HWND window = nullptr;
 HWND comboBox1 = nullptr;
 HWND comboBox2 = nullptr;
 HWND comboBox3 = nullptr;
@@ -30,7 +30,7 @@ LRESULT OnComboboxChange(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  if (message == WM_CLOSE && hwnd == form) return OnWindowClose(hwnd, message, wParam, lParam);
+  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
   if (message == WM_COMMAND && HIWORD(wParam) == CBN_SELCHANGE && (HWND)lParam == comboBox1) return OnComboboxChange(hwnd, message, wParam, lParam);
   if (message == WM_COMMAND && HIWORD(wParam) == CBN_SELCHANGE && (HWND)lParam == comboBox2) return OnComboboxChange(hwnd, message, wParam, lParam);
   if (message == WM_COMMAND && HIWORD(wParam) == CBN_SELCHANGE && (HWND)lParam == comboBox3) return OnComboboxChange(hwnd, message, wParam, lParam);
@@ -38,10 +38,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 }
 
 int main() {
-  form = CreateWindowEx(0, WC_DIALOG, L"ComboBox example", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
-  comboBox1 = CreateWindowEx(0, WC_COMBOBOX, L"", WS_CHILD | WS_VISIBLE | CBS_DROPDOWN, 10, 10, 120, 21, form, nullptr, nullptr, nullptr);
-  comboBox2 = CreateWindowEx(0, WC_COMBOBOX, L"", WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, 10, 50, 120, 21, form, nullptr, nullptr, nullptr);
-  comboBox3 = CreateWindowEx(0, WC_COMBOBOX, L"", WS_CHILD | WS_VISIBLE | CBS_SIMPLE, 10, 90, 120, 170, form, nullptr, nullptr, nullptr);
+  window = CreateWindowEx(0, WC_DIALOG, L"ComboBox example", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
+  comboBox1 = CreateWindowEx(0, WC_COMBOBOX, L"", WS_CHILD | WS_VISIBLE | CBS_DROPDOWN, 10, 10, 120, 21, window, nullptr, nullptr, nullptr);
+  comboBox2 = CreateWindowEx(0, WC_COMBOBOX, L"", WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, 10, 50, 120, 21, window, nullptr, nullptr, nullptr);
+  comboBox3 = CreateWindowEx(0, WC_COMBOBOX, L"", WS_CHILD | WS_VISIBLE | CBS_SIMPLE, 10, 90, 120, 170, window, nullptr, nullptr, nullptr);
 
   for (auto item : { L"Item1", L"Item2", L"Item3", L"Item4", L"Item5", L"Item6", L"Item7", L"Item8", L"Item9", L"Item10" }) {
     SendMessage(comboBox1, CB_ADDSTRING, 0, (LPARAM)item);
@@ -52,8 +52,8 @@ int main() {
   SendMessage(comboBox2, CB_SETCURSEL, 1, 0);
   SendMessage(comboBox3, CB_SETCURSEL, 1, 0);
 
-  defWndProc = (WNDPROC)SetWindowLongPtr(form, GWLP_WNDPROC, (LONG_PTR)WndProc);
-  ShowWindow(form, SW_SHOW);
+  defWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+  ShowWindow(window, SW_SHOW);
 
   MSG message = { 0 };
   while (GetMessage(&message, nullptr, 0, 0)) {
