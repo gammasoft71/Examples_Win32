704cb7275da0bb8963553644c62cc7fb9b15700b
gammasoft71 <gammasoft71@gmail.com>
2021-04-25 23:30:27 +0200

    Update HelloWorldMessageBox.cpp

--- Win32.Gui/HelloWorlds/HelloWorldMessageBox/HelloWorldMessageBox.cpp
+++ Win32.Gui/HelloWorlds/HelloWorldMessageBox/HelloWorldMessageBox.cpp
@@ -7,15 +7,24 @@ HWND window = nullptr;
 HWND button = nullptr;
 WNDPROC defWndProc = nullptr;
 
+LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  PostQuitMessage(0);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
+LRESULT OnButtonClick(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  MessageBox(window, L"Hello, World!", L"", MB_OK);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);;
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  if (message == WM_CLOSE && hwnd == window) PostQuitMessage(0);
-  if (message == WM_COMMAND && HIWORD(wParam) == BN_CLICKED && (HWND)lParam == button)
-    MessageBox(window, L"Hello, World!", L"", MB_OK);
+  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
+  if (message == WM_COMMAND && HIWORD(wParam) == BN_CLICKED && (HWND)lParam == button) return OnButtonClick(hwnd, message, wParam, lParam);
   return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
 }
 
 int main() {
-  window = CreateWindowEx(0, WC_DIALOG, L"Hello world (message)", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 316, 330, nullptr, nullptr, nullptr, nullptr);
+  window = CreateWindowEx(0, WC_DIALOG, L"Hello world (MessageBox)", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 316, 330, nullptr, nullptr, nullptr, nullptr);
   button = CreateWindowEx(0, WC_BUTTON, L"&Click me", WS_CHILD | WS_VISIBLE, 10, 10, 75, 25, window, nullptr, nullptr, nullptr);
 
   defWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
