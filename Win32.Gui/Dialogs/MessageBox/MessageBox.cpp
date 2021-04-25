73024e1dd9a9a833736fd8dcc0035e13e376262d
gammasoft71 <gammasoft71@gmail.com>
2021-04-25 23:24:47 +0200

    Update MessageBox.cpp

--- Win32.Gui/Dialogs/MessageBox/MessageBox.cpp
+++ Win32.Gui/Dialogs/MessageBox/MessageBox.cpp
@@ -10,8 +10,13 @@ using namespace std::literals;
 
 HWND window = nullptr;
 HWND buttonShowMessage = nullptr;
-HWND labelDiallogResult = nullptr;
+HWND staticTextDialogResult = nullptr;
 WNDPROC defWndProc = nullptr;
+
+LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  PostQuitMessage(0);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
 
 wstring DialogResultTostring(int32_t ddialogResult) {
   static map<int32_t, wstring> dialogResults{{IDABORT, L"Abort"}, {IDCANCEL, L"Cancel"}, {IDCLOSE, L"Close"}, {IDCONTINUE, L"Contnue"}, {IDHELP, L"Hellp"}, {IDIGNORE, L"Ignore"}, {IDNO, L"No"}, {IDOK, L"OK"}, {IDRETRY, L"Retry"}, {IDTIMEOUT, L"Timeout"}, {IDTRYAGAIN, L"TryAgain"}, {IDYES, L"Yes"}};
@@ -19,19 +24,22 @@ wstring DialogResultTostring(int32_t ddialogResult) {
   return it != dialogResults.end() ? it->second : L"(unknown)";
 }
 
+LRESULT OnButtonClick(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  wstring result = L"DialogResult = "s + DialogResultTostring(MessageBox(window, L"Hello, World!", L"Message", MB_OKCANCEL | MB_ICONWARNING));
+  SendMessage(staticTextDialogResult, WM_SETTEXT, 0, (LPARAM)result.c_str());
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  if (message == WM_CLOSE && hwnd == window) PostQuitMessage(0);
-  if (message == WM_COMMAND && HIWORD(wParam) == BN_CLICKED && (HWND)lParam == buttonShowMessage) {
-    wstring result = L"DialogResult = "s + DialogResultTostring(MessageBox(window, L"Hello, World!", L"Message", MB_OKCANCEL|MB_ICONWARNING));
-    SendMessage(labelDiallogResult, WM_SETTEXT, 0, (LPARAM)result.c_str());
-  }
+  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
+  if (message == WM_COMMAND && HIWORD(wParam) == BN_CLICKED && (HWND)lParam == buttonShowMessage) return OnButtonClick(hwnd, message, wParam, lParam);
   return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
 }
 
 int main() {
   window = CreateWindowEx(0, WC_DIALOG, L"Message box exemple", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
   buttonShowMessage = CreateWindowEx(0, WC_BUTTON, L"Message...", WS_CHILD | WS_VISIBLE, 10, 10, 80, 25, window, nullptr, nullptr, nullptr);
-  labelDiallogResult = CreateWindowEx(0, WC_STATIC, L"", WS_CHILD | WS_VISIBLE, 10, 45, 200, 23, window, nullptr, nullptr, nullptr);
+  staticTextDialogResult = CreateWindowEx(0, WC_STATIC, L"", WS_CHILD | WS_VISIBLE, 10, 45, 200, 23, window, nullptr, nullptr, nullptr);
   
   defWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
   ShowWindow(window, SW_SHOW);
