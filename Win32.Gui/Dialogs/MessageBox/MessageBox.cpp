17df97fdcf4927c5630c117c56754190756cd7d4
gammasoft71 <gammasoft71@gmail.com>
2021-04-29 15:43:28 +0200

    Update MessageBox.cpp

--- Win32.Gui/Dialogs/MessageBox/MessageBox.cpp
+++ Win32.Gui/Dialogs/MessageBox/MessageBox.cpp
@@ -37,7 +37,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 }
 
 int main() {
-  window = CreateWindowEx(0, WC_DIALOG, L"Message box exemple", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
+  window = CreateWindowEx(0, WC_DIALOG, L"MessageBox exemple", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
   buttonShowMessage = CreateWindowEx(0, WC_BUTTON, L"Message...", WS_CHILD | WS_VISIBLE, 10, 10, 80, 25, window, nullptr, nullptr, nullptr);
   staticTextDialogResult = CreateWindowEx(0, WC_STATIC, L"", WS_CHILD | WS_VISIBLE, 10, 45, 200, 23, window, nullptr, nullptr, nullptr);
   
