7a4158a0ff6715fbc79c6bac7bb131194b6e4a3b
gammasoft71 <gammasoft71@gmail.com>
2021-04-29 16:49:20 +0200

    add FolderBrowser example

---
+++ Win32.Gui/Dialogs/FolderBrowser/FolderBrowser.cpp
@@ -0,0 +1,58 @@
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <map>
+#include <string>
+#include <Windows.h>
+#include <CommCtrl.h>
+#include <ShlObj.h>
+
+using namespace std;
+using namespace std::literals;
+
+HWND window = nullptr;
+HWND buttonShowMessage = nullptr;
+HWND staticText1 = nullptr;
+WNDPROC defWndProc = nullptr;
+wstring selectedPath;
+
+LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  PostQuitMessage(0);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
+LRESULT OnButtonClick(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  BROWSEINFO browserInfo = { 0 };
+  browserInfo.hwndOwner = hwnd;
+  browserInfo.lParam = reinterpret_cast<LPARAM>(selectedPath.c_str());
+  browserInfo.lpszTitle = L"Select a folder";
+
+  browserInfo.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;
+
+  PCIDLIST_ABSOLUTE result = SHBrowseForFolder(&browserInfo);
+  if (result) {
+    wchar_t path[MAX_PATH];
+    SHGetPathFromIDList(result, path);
+    selectedPath = path;
+    SendMessage(staticText1, WM_SETTEXT, 0, reinterpret_cast<LPARAM>(selectedPath.c_str()));
+  }
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
+  if (message == WM_COMMAND && HIWORD(wParam) == BN_CLICKED && reinterpret_cast<HWND>(lParam) == buttonShowMessage) return OnButtonClick(hwnd, message, wParam, lParam);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
+int main() {
+  window = CreateWindowEx(0, WC_DIALOG, L"FolderBrowser exemple", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
+  buttonShowMessage = CreateWindowEx(0, WC_BUTTON, L"Folder...", WS_CHILD | WS_VISIBLE, 10, 10, 80, 25, window, nullptr, nullptr, nullptr);
+  staticText1 = CreateWindowEx(0, WC_STATIC, L"", WS_CHILD | WS_VISIBLE, 10, 45, 274, 215, window, nullptr, nullptr, nullptr);
+  
+  defWndProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(window, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WndProc)));
+  ShowWindow(window, SW_SHOW);
+
+  MSG message = {0};
+  while (GetMessage(&message, nullptr, 0, 0))
+    DispatchMessage(&message);
+}
---
+++ Win32.Gui/Dialogs/FolderBrowser/README.md
@@ -0,0 +1,25 @@
+# FolderBrowser
+
+This example demonstrates the use of FolderBrowser dialog.
+
+# Source
+
+[FolderBrowser.cpp](FolderBrowser.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Generate and build
+
+To build this project, open "Console" and type following lines:
+
+``` shell
+mkdir build && cd build
+cmake .. 
+start FolderBrowser.sln
+```
+
+Select "FolderBrowser" project and type Ctrl+F5 to build and run it.
+
+# Output
+
+![Screenshot](../../../docs/Pictures/FolderBrowser.png)
